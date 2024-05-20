8847e80b8331dc6c94216227d2089fe4e417fd1b
Matias <akucfh@gmail.com>
2024-05-20 16:03:43 +0200

    first commit

---
+++ s15-10/demo/src/test/java/s1510/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package s1510.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

4f7477d5036cab6f907b76b1362dab897a373a82
nedder3 <arij.dev@gmail.com>
2024-05-24 11:48:35 -0300

    add Match entity properties

--- s15-10/demo/src/main/java/s1510/demo/model/Match.java
+++ s15-10/demo/src/main/java/s1510/demo/model/Match.java
@@ -1,4 +1,20 @@
 package s1510.demo.model;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Match {
+    Date startAt;
+    Date endAt;
+    Integer pointsTeamA;
+    Integer pointsTeamB;
+    Sport sport;
 }

86261206cb0a39b615e07ad40a9e9743d15379be
nedder3 <arij.dev@gmail.com>
2024-05-25 04:58:59 -0300

    delete validations in MatchResponseDto

--- s15-10/demo/src/main/java/s1510/demo/dtos/response/MatchResponseDto.java
+++ s15-10/demo/src/main/java/s1510/demo/dtos/response/MatchResponseDto.java
@@ -6,18 +6,19 @@ import lombok.experimental.FieldDefaults;
 import s1510.demo.model.Sport;
 
 import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public record MatchResponseDto( @NotNull(message = "La fecha de inicio no puede ser nula")
-                                LocalDateTime startAt,
-                                @NotNull(message = "La fecha de finalización no puede ser nula")
-                                LocalDateTime endAt,
-                                @NotNull(message = "Los puntos del equipo A no pueden ser nulos")
-                                Integer pointsTeamA,
-                                @NotNull(message = "Los puntos del equipo B no pueden ser nulos")
-                                Integer pointsTeamB,
-                                @NotNull(message = "El deporte no puede ser nulo")
-                                Sport sport) {
+public record MatchResponseDto(
+        LocalDateTime startAt,
+
+        LocalDateTime endAt,
+
+        Integer pointsTeamA,
+
+        Integer pointsTeamB,
+
+        Sport sport) {
 
 }

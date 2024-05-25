03d373a89cb5e7b07fef91c89584b5897f8ccf38
nedder3 <arij.dev@gmail.com>
2024-05-25 04:21:25 -0300

    add MatchController

---
+++ s15-10/demo/src/main/java/s1510/demo/controller/MatchController.java
@@ -0,0 +1,44 @@
+package s1510.demo.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import s1510.demo.dtos.request.MatchRequestDTO;
+import s1510.demo.dtos.response.MatchResponseDto;
+
+import s1510.demo.service.MatchService;
+
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "match")
+public class MatchController {
+    private final MatchService matchService;
+
+
+    @Autowired
+    public MatchController(MatchService matchService) {
+        this.matchService = matchService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MatchResponseDto>> getAllMatches() {
+        List<MatchResponseDto> matchList = matchService.getAllMatches();
+        return new ResponseEntity<>(matchList, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<MatchResponseDto> createMatch(@RequestBody MatchRequestDTO matchRequestDTO) {
+        MatchResponseDto match = matchService.createMatch(matchRequestDTO);
+        return new ResponseEntity<>(match, HttpStatus.CREATED);
+
+    }
+
+    @GetMapping("{/id}")
+    public ResponseEntity<MatchResponseDto> getMatchById(@PathVariable Integer id, @RequestBody MatchRequestDTO matchRequestDTO) {
+        MatchResponseDto updatedMatch = matchService.getMatchById(id);
+        return new ResponseEntity<>(updatedMatch, HttpStatus.OK);
+    }
+
+}

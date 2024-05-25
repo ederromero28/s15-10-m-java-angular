08f05addf24d8941920d075f0248c4fcc535a03e
nedder3 <arij.dev@gmail.com>
2024-05-25 01:05:05 -0300

    add MatchRequestDTO with validations

---
+++ s15-10/demo/src/main/java/s1510/demo/dtos/request/MatchRequestDTO.java
@@ -0,0 +1,28 @@
+package s1510.demo.dtos.request;
+
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public record MatchRequestDTO(
+        @NotNull(message = "La fecha de inicio no puede ser nula")
+        Date startAt,
+        @NotNull(message = "La fecha de fin no puede ser nula")
+        @FutureOrPresent(message = "La fecha de fin debe ser en el presente o futuro")
+        Date endAt,
+        @NotNull(message = "Los puntos del equipo A no pueden ser nulos")
+        @Min(value = 0, message = "Los puntos del equipo A no pueden ser negativos")
+        Integer pointsTeamA,
+        @NotNull(message = "Los puntos del equipo B no pueden ser nulos")
+        @Min(value = 0, message = "Los puntos del equipo B no pueden ser negativos")
+        Integer getPointsTeamB,
+        @NotNull(message = "El ID del deporte no puede ser nulo")
+        @Positive(message = "El ID del deporte debe ser un número positivo")
+        Long sportId) {
+
+}

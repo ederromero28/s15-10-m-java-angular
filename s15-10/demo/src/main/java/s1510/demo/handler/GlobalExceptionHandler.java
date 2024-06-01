0105e532af7c0faa5cb3aedbc788075a7d52ab74
nedder3 <arij.dev@gmail.com>
2024-06-01 19:00:11 -0300

    add ExceptionHandler w/controllerAdvice for exceptions

---
+++ s15-10/demo/src/main/java/s1510/demo/handler/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package s1510.demo.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import s1510.demo.dtos.error.ErrorDTO;
+import s1510.demo.exception.BadRequestException;
+import s1510.demo.exception.ResourceNotFoundException;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDTO> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+        ErrorDTO errorDTO = new ErrorDTO(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDTO, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorDTO> handleBadRequestException(BadRequestException ex, WebRequest request) {
+        ErrorDTO errorDTO = new ErrorDTO(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDTO, HttpStatus.BAD_REQUEST);
+    }git 
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDTO> handleGlobalException(Exception ex, WebRequest request) {
+        ErrorDTO errorDTO = new ErrorDTO(LocalDateTime.now(), "An unespected error ocurred", request.getDescription(false));
+        return new ResponseEntity<>(errorDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
